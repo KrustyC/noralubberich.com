51db9a9fbd198a680d075b111844cd26d6aa50f0
Davide Crestini <davide.crestini94@gmail.com>
2022-06-01 17:03:11 +0100

    feat: updated images

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,14 +1,14 @@
 import yogaPic from "../../public/images/yoga.jpg";
 import corporateWellnessPic from "../../public/images/corporate-wellness.jpg";
-import nutritionPic from "../../public/images/nutrition.png";
+import nutritionPic from "../../public/images/nutrition.jpeg";
+import aboutMePic from "../../public/images/about-me.jpg";
 
 export const PAGE_LINKS = [
-    {label: 'About Me', to: 'about-me'},
-    {label: 'Corporate Wellness', to: 'corporate-wellness'},
-    {label: 'Yoga', to: 'yoga'},
-    {label: 'Nutrition', to: 'nutrition'},
+  { label: "About Me", to: "about-me" },
+  { label: "Corporate Wellness", to: "corporate-wellness" },
+  { label: "Yoga", to: "yoga" },
+  { label: "Nutrition", to: "nutrition" },
 ];
-
 
 export const ACTIVITIES = [
   { label: "Yoga", href: "/yoga", pic: yogaPic },
@@ -18,7 +18,7 @@ export const ACTIVITIES = [
     pic: corporateWellnessPic,
   },
   { label: "Nutrition", href: "/nutrition", pic: nutritionPic },
-  { label: "About Me", href: "/about-me", pic: yogaPic },
+  { label: "About Me", href: "/about-me", pic: aboutMePic },
 ];
 
 export const INSTAGRAM_LINK = "https://www.instagram.com/nora.lubberich/";
