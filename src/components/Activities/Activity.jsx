421a1611cb176b70bbc451a20776bbda0a230ace
Davide Crestini <davide.crestini94@gmail.com>
2022-06-01 16:26:39 +0100

    feat: activity

---
+++ src/components/Activities/Activity.jsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export const Activity = ({ label, href, imgSrc }) => (
+  <Link href={href}>
+    <a>
+      <div className="h-[650px] w-full relative">
+        <Image
+          src={imgSrc}
+          alt={label}
+          placeholder="blur"
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+        />
+
+        <div className="absolute top-[300px] left-[50px]">
+          <p className="underline uppercase font-bold text-4xl text-white">
+            {label}
+          </p>
+        </div>
+      </div>
+    </a>
+  </Link>
+);
---
+++ src/components/Activities/index.jsx
@@ -0,0 +1,48 @@
+import { useRef, useState, useEffect } from "react";
+import { animated, config, useSprings } from "@react-spring/web";
+import { useIsVisible } from "@/hooks/useIsVisible";
+import { ACTIVITIES } from "@/utils/constants";
+
+import { Activity } from "./Activity";
+
+export const Activities = () => {
+  const ref = useRef();
+  const isVisible = useIsVisible(ref, 0.2);
+  const [open, setOpen] = useState(false);
+
+  const configs = ACTIVITIES.map((item, i) => ({
+    delay: 500 * i,
+    config: config.gentle,
+    from: { opacity: 0, x: 50, y: 400 },
+    to: {
+      opacity: open ? 1 : 0,
+      x: open ? 0 : 50,
+      y: open ? 0 : 400,
+      transform: "translateY(0px)",
+    },
+  }));
+
+  const springs = useSprings(4, configs);
+
+  useEffect(() => {
+    if (isVisible && !open) {
+      setOpen(true);
+    }
+  }, [isVisible, open]);
+
+  return (
+    <section ref={ref} className="px-8 lg:px-60 grid grid-cols-2 gap-16">
+      {springs.map(({ ...style }, i) => {
+        const { label, pic, href } = ACTIVITIES[i];
+
+        return (
+          <animated.div key={i} style={style}>
+            <animated.div>
+              <Activity label={label} href={href} imgSrc={pic} />
+            </animated.div>
+          </animated.div>
+        );
+      })}
+    </section>
+  );
+};
